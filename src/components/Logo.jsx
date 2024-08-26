ac3707512b0de3ed887ad7b1996effc1f97056ea
Zeel Patel <patelzeel044@gmail.com>
2024-08-26 12:39:12 +0000

    rebranding

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../assets/572.png'
 
 function Logo({width = '100px'}) {
-    return <div className='lg:w-20 h-8 flex items-center -my-2 px-2 p-1  md:w-16 sm:w-20'><p className='text-2xl font-bold mx-1'>My</p><img src={logo} width={width} alt="Logo"/></div>
+    return <div className='lg:w-20 h-8 flex items-center -my-2 px-2 p-1  md:w-16 sm:w-20'><img src={logo} width={width} alt="Logo"/><p className='text-2xl font-bold mx-1'>Space</p></div>
 }
 
 export default Logo
